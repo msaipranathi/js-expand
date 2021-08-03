43eb641e7b42fc5511f16f2dbc6786517707f7ba
msaipranathi <45840010+msaipranathi@users.noreply.github.com>
2021-08-03 20:28:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const acc = document.getElementById('contentBx');
+const acc1 = document.getElementById('contentBx1');
+const acc2 = document.getElementById('contentBx2');
+const acc3 = document.getElementById('contentBx3');
+
+    acc.addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+    acc1.addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+    acc2.addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+    acc3.addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+
